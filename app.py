0ae743414936cf090d92d003ba1b54fc5f643c65
JoelMadd <134186317+joevinMadd@users.noreply.github.com>
2024-09-06 18:48:28 +0800

    index.html

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask("OMR")
 
 @app.route('/', methods = ['GET'])
 def home():
-    return "Hello, Monka!"
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
